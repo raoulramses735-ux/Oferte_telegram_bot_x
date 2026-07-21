d2bf4b7ad8826551f03cbde8c13fbf24ef014162
raoulramses735-ux <raoulramses735@gmail.com>
2026-07-21 11:33:16 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,7 @@
+from telegram import Bot
+
+TOKEN = "8938583105:AAETSSBD8dWk0rgAy_i-ogTNJSFVo8U8VLo"
+
+bot = Bot(token=TOKEN)
+
+bot.send_message(chat_id="oferte_romania_zilnic", text="Salut! Botul funcționează! 🎉")
